03653b7735a157af13a44c54941925f54906f8ab
Michael Huang <machairodus.hzy@gmail.com>
2018-11-26 18:12:25 +0800

    Use fresnel example
    Need to implement the board tracing!

--- js/generator.js
+++ js/generator.js
@@ -1,9 +1,13 @@
-'use strict'
-
 
 const Shader = {
     Phong: new THREE.MeshPhongMaterial({ color: 0x00ff00 }),
-    Frensel: new THREE.ShaderMaterial(),
+    Fresnel: 
+        new THREE.ShaderMaterial( {
+            uniforms: THREE.FresnelShader.uniforms,
+            vertexShader: THREE.FresnelShader.vertexShader,
+            fragmentShader: THREE.FresnelShader.fragmentShader
+        } )
+    ,
     Basic: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 };
 
@@ -11,20 +15,22 @@ class CrystalGenerator {
 
     constructor(shader) {
         if (shader)
-            this.shader = params.Shader;
+            this.shader = params.shader;
         else
-            this.shader = Shader.Basic;
+            this.shader = Shader.Fresnel;
     }
 
     generate(scene) {
-        var geo = new THREE.OctahedronGeometry(100, 0);
+        var geo = new THREE.OctahedronGeometry(250, 0);
         var material = new Physijs.createMaterial(
             this.shader,
-            0.8,
+            0,
             0.3
         );
-        var object = new Physijs.BoxMesh(geo, material);
+        var object = new Physijs.ConvexMesh(geo, material);
         object.position.set(Math.random() * 5000 - 2500, 1000, Math.random() * 5000 - 2500);
+        object.rotation.set(Math.random()*(Math.PI),Math.random()*(Math.PI),Math.random()*(Math.PI));
+        object.setLinearVelocity(new THREE.Vector3(0,500,500));
         scene.add(object);
     }
 }
