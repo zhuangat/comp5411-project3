4dc5999c07afbdc6fa0ea3d71120f4b28550ecd0
Michael Huang <machairodus.hzy@gmail.com>
2018-11-28 02:52:14 +0800

    Add texture cube variance

--- js/generator.js
+++ js/generator.js
@@ -1,14 +1,28 @@
 
 const Shader = {
-    Fresnel: 
-        new THREE.ShaderMaterial( {
+    Fresnel:
+        new THREE.ShaderMaterial({
             uniforms: THREE.FresnelShader.uniforms,
             vertexShader: THREE.FresnelShader.vertexShader,
             fragmentShader: THREE.FresnelShader.fragmentShader,
-        } )
+        })
     ,
     Basic: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 };
+
+const geos = [
+    new THREE.OctahedronGeometry(250, 0),
+    new THREE.OctahedronGeometry(250, 1),
+    //new THREE.OctahedronGeometry(250, 2),
+    new THREE.DodecahedronGeometry(250, 1),
+    //new THREE.DodecahedronGeometry(250, 2),
+    new THREE.IcosahedronBufferGeometry(250, 0),
+    new THREE.IcosahedronBufferGeometry(250, 1),
+    //new THREE.IcosahedronBufferGeometry(250, 2),
+    new THREE.TetrahedronGeometry(250, 0),
+    new THREE.TetrahedronGeometry(250, 1),
+    new THREE.TetrahedronGeometry(250, 2),
+];
 class CrystalGenerator {
 
     constructor(shader) {
@@ -18,9 +32,8 @@ class CrystalGenerator {
             this.shader = Shader.Fresnel;
     }
 
-    generate(scene, loader) {
-        var geo = new THREE.OctahedronGeometry(250, 2);
-        // var geo = THREE.Geometry().fromBufferGeometry(geos[-1]);
+    generate(scene) {
+        var geo = geos[Math.floor(Math.random() *geos.length)];;
         var material = new Physijs.createMaterial(
             this.shader,
             0,
@@ -28,8 +41,8 @@ class CrystalGenerator {
         );
         var object = new Physijs.ConvexMesh(geo, material);
         object.position.set(Math.random() * 5000 - 2500, 1000, Math.random() * 5000 - 2500);
-        object.rotation.set(Math.random()*(Math.PI),Math.random()*(Math.PI),Math.random()*(Math.PI));
-        object.setLinearVelocity(new THREE.Vector3(0,500,500));
+        object.rotation.set(Math.random() * (Math.PI), Math.random() * (Math.PI), Math.random() * (Math.PI));
+        object.setLinearVelocity(new THREE.Vector3(0, 500, 500));
         object.castShadow = true;
         scene.add(object);
     }
